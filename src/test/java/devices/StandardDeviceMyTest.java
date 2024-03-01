b44af435569845f6d8c11579807709a98d828522
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2024-03-01 12:43:24 +0100

    test StandardDevice is off after creation

--- src/test/java/devices/StandardDeviceMyTest.java
+++ src/test/java/devices/StandardDeviceMyTest.java
@@ -3,13 +3,23 @@ package devices;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.*;
 
 public class StandardDeviceMyTest {
 
+    private FailingPolicy dummyFailingPolicy;
+    private StandardDevice device;
+
+    @BeforeEach
+    public void createDummyFailingPolicy(){
+        this.dummyFailingPolicy = mock(FailingPolicy.class);
+        this.device = new StandardDevice(dummyFailingPolicy);
+    }
+
     @Test
-    @DisplayName("StandardDevice creation withou failing policy")
+    @DisplayName("StandardDevice creation without failing policy")
     public void testNullFailingPolicy(){
         assertThrows(NullPointerException.class, () -> new StandardDevice(null));
     }
@@ -17,8 +27,13 @@ public class StandardDeviceMyTest {
     @Test
     @DisplayName("StandardDevice creation with failing policy")
     public void testDeviceCreationWithFailingPolicy(){
-        final FailingPolicy dummyFailingPolicy = mock(FailingPolicy.class);
-        assertDoesNotThrow(() -> new StandardDevice(dummyFailingPolicy));
+        assertDoesNotThrow(() -> new StandardDevice(this.dummyFailingPolicy));
+    }
+
+    @Test
+    @DisplayName("StandardDevice is off after creation")
+    public void testIsOffAtCreation(){
+        assertFalse(this.device.isOn());
     }
 
 
